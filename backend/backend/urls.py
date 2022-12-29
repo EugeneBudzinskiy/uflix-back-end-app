e291eb80d250c77576213d7511a776a67b9ef13e
EugeneBudzinskiy <eugene.budzinskiy@gmail.com>
2022-12-29 13:31:28 +0200

    Clean up

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
-# import views from test
 # noinspection PyUnresolvedReferences
 from uflix import views
 
